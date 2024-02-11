23b955fe873079403c29e419e934deca83a495bc
kudr.max <mdkudryashov@edu.hse.ru>
2024-02-11 13:05:35 +0300

    Добавил конспект с видоса "https://youtu.be/OZOOLe2imFo"

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import matplotlib.pyplot as plt
+
+heads_tails = [0, 0]
+
+for _ in range(10000):
+    heads_tails[random.randint(0, 1)] += 1
+    plt.bar(['Heads', 'Tails'], heads_tails, color=['red', 'blue'])
+    plt.pause(0.001)
+plt.show()
